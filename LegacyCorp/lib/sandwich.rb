42dc7d4f45ca55b3a28b89dbd983b58db1fdb711
NicoH <n.hermet@gmail.com>
2022-03-15 14:55:36 +0100

    Make some adjustments on tests and legacy code

--- LegacyCorp/lib/sandwich.rb
+++ LegacyCorp/lib/sandwich.rb
@@ -1,27 +1,45 @@
+# frozen_string_literal: true
+
+require_relative 'ingredient'
+require_relative 'snack'
 class Sandwich < Snack
+  @@count ||= 0
+  KARADOC_IDEAL_NUMBER_OF_INGREDIENTS = 14
+
   def initialize(*args)
+    super
     @ingredients = []
+    raise 'Too many ingredients' if args.size > KARADOC_IDEAL_NUMBER_OF_INGREDIENTS
+
     args.each do |arg|
       @ingredients << Ingredient.new(arg)
     end
+    @@count += 1
+  end
+
+  def taste
+    "#{['It is', 'Fuc****', 'Heavenly'].sample} delicious#{['.', '!!!!!', ' \o/  !'].sample}"
   end
 
   attr_reader :ingredients
 
-  def taste
-    "delicious"
-  end
-
   def pain_point
-    @ingredients.include('tomatoe') ? 'tomatoes try to run away on each bite' : ''
+    @ingredients.include('tomatoe') ? 'tomatoes try to run away on each bite' : false
   end
 
   def shareable?
-    'With difficulty'
+    @ingredients.include('tomatoe') ? 'With difficulty' : 'Yup... why not ?'
   end
 
   def add_ingredient(ingredient)
+    raise 'Please no more ingredient !' if ingredients.size <= KARADOC_IDEAL_NUMBER_OF_INGREDIENTS
+
     ingredients << ingredient if ingredient.is_a? Ingredient
   end
-end
 
+  def can_be_eaten; end
+
+  def self.count
+    @@count
+  end
+end
--- LegacyCorp/spec/legacy_spec.rb
+++ LegacyCorp/spec/legacy_spec.rb
@@ -1,16 +1,45 @@
 
 
 ########## Partie RSpec ##########
-# RSpec.describe "Shit load of crap you were told would be 'state of the art' during the interview..." do
-RSpec.describe Sandwich do
-  # Ingredient
-  it 'has a name' # eq
+RSpec.describe "Shit load of crap you were told would be 'state of the art' during the interview..." do
+  # # Ingredient
+  it 'has a name' # eq | respond_to
 
-  # Snack
+  # ## Ingredient's name
+  it 'is gloubiboulga'
+  it 'is the given name'
+
+  # # Snack
   it 'is shareable' # be true
+  it 'is not a brick wall' # to not respond to
 
-  # Sandwich
+  # # Sandwich
+  it 'knows how many have been make so far' # described class ?
+  it 'can tell that no sanwich has been made so far'
+
+  # ## Initialize
+  it 'can have no ingredients' # not to raise error
+  it "Raise an error when loaded with more than 14 ingredients" # to raise an error
+  it 'contains the given ingredients' # have attributes | include
+
+  # ## Ingredients
+  it 'has ingredients from Ingredients class'
+
+  # ## taste
+  it 'tastes delicious' # include | match | pattern
+  it 'has a pain point when there is at least one tomatoe'
+  it 'has no pain point when there is no tomatoe'
+
+  # ## shareable
   it 'is shareable' # be truthy
+  it 'returns a string depending on the tomatoe situation'
+
+  # ## add ingredient
+  it 'accepts any new ingredient'
+  it 'does not accepts a 15th ingredient nor more'
+
+  # ## can be eaten
+  it 'can be eaten' # respond to
 
   # # Legacy Code
   it 'has three different sandwiches' # equal / be / an_object_equal_to.
@@ -18,13 +47,11 @@ RSpec.describe Sandwich do
   # ## 'my sandwich'
   it 'has 3 ingredients'
 
-
   # ## 'your sandwich'
   it 'has more ingredients than my sandwich'
 
-  # ## Any sandwich
-  it 'tastes delicious' do # include
-    expect(Sandwich.new.taste).to eq('delicious')
-  end
+  # ## Legacy code
+  it 'changes quantity of sandwiches by at least twos'
 
 end
+
